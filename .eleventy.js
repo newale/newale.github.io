39a2e41c5cce1ad9af8a7423799c66d1ac710131
Alejandro Bustos <alejandro.bustos.n@gmail.com>
2025-08-15 15:21:19 -0400

    feat: update layouts and other stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static/css/main.css");
-  eleventyConfig.addCollection("postsByCategoria", function(collectionApi) {
-    const posts = collectionApi.getFilteredByTag("post").sort((a, b) => b.date - a.date);
-    const grouped = {};
-    for (const post of posts) {
-      const categoria = post.data.categoria || "Sin categoría";
-      if (!grouped[categoria]) grouped[categoria] = [];
-      grouped[categoria].push(post);
-    }
-    return grouped;
-  });
   eleventyConfig.addCollection("categorias", function(collectionApi) {
     const posts = collectionApi.getFilteredByTag("post").sort((a, b) => b.date - a.date);
     const categorias = [];
