dfe4e468b79aacb74f11a2f788a12c8fcb910c0c
Alejandro Bustos <alejandro.bustos.n@gmail.com>
2025-09-16 23:40:41 -0300

    feat: update site and add otto

--- packages/main-site/.eleventy.js
+++ packages/main-site/.eleventy.js
@@ -1,14 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static/css/main.css");
-
-  eleventyConfig.addCollection("categorias", function(collectionApi) {
-    const posts = collectionApi.getFilteredByTag("post").sort((a, b) => b.date - a.date);
-    const categorias = [];
-    for (const post of posts) {
-      const categoria = post.data.categoria || "Sin categoría";
-      if (!categorias.includes(categoria)) categorias.push(categoria);
-    }
-    console.log("Categorías:", categorias);
-    return categorias;
-  });
+  eleventyConfig.addPassthroughCopy("static/images");
 }
---
+++ packages/main-site/contenido/imagenes/otto.md
@@ -0,0 +1,12 @@
+---
+title: "Otto"
+date: 2024-11-02
+tag: "arte digital"
+categoria: "arte"
+layout: "layouts/art.html"
+---
+
+<img src="/static/images/Untitled_Artwork_2.png" alt="arte digital, obra: otto">
+
+
+
